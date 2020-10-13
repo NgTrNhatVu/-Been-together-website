f8b6c7e590a6a65379908a14dc292f92fee17572
Nguyen Tran Nhat Vu <61381778+NgTrNhatVu@users.noreply.github.com>
2020-10-13 18:58:58 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,47 +4,53 @@ function getTime()
 	var startMonth = document.getElementById("startMonth").value;
 	startMonth--;
 	var startYear = document.getElementById('startYear').value;
-	var startDate = new Date(startYear ,startMonth, startDay);  
-	return startDate;
+	return new Date(startYear ,startMonth, startDay); 
 }
 function daysCalculate()
 {
-	var startDate = getTime();
-	var oneDay = 1000 * 60 * 60 * 24; //miliseconds * seconds * minutes * hour = 1 day
-	var today = new Date();
-	var days = (today - startDate) / oneDay;//how many days have passed
-	return days;
+    const MILLISECOND_PER_DAY = 1000 * 60 * 60 * 24; //milliseconds * seconds * minutes * hour = 1 day
+
+    let startDate = getTime();
+    var today = new Date();
+
+    if(today < startDate){
+        alert("Please chose the right date!");
+        return false;
+    }
+
+	return (today - startDate) / MILLISECOND_PER_DAY;//get how many days have passed
 }
 function Calculate()
 {
-	var days = daysCalculate();
-	if(days < 0)
-	{
-		alert("please chose the right date!");
-		return false;
-	}
+    const DAYS_PER_YEAR = 365;
+    const DAYS_PER_MONTH = 30;
+    
+    var days = daysCalculate();
+    
 	//change the number of days to years-months-days form
-	var trueYear = days / 365;
-	var milisecondMonth = days % 365;
-	var trueMonth = milisecondMonth / 30;
-	var trueDay = milisecondMonth % 30;
-	var dateResult = parseInt(trueYear) + " years " + parseInt(trueMonth) + " months " + parseInt(trueDay) + " days "; 
-	return dateResult;
+	var trueYear = days / DAYS_PER_YEAR;
+    var remainMonth = days % DAYS_PER_YEAR;
+    
+	var trueMonth = remainMonth / DAYS_PER_MONTH;
+    var trueDay = remainMonth % DAYS_PER_MONTH;
+    
+	var dateMessage = parseInt(trueYear) + " years " + parseInt(trueMonth) + " months " + parseInt(trueDay) + " days ";
+	return dateMessage;
 }
 function displayResult()
 {
-	var show = document.getElementById('wrapped-display');
-	show.style.display = 'block';
-	var hidden = document.getElementById('wrapped-index');
-	hidden.style.display = 'none';
-	var dateResult = Calculate();
+    let dateResult = Calculate();
 	document.getElementById('result').innerHTML = dateResult;
+	let show = document.getElementById('display-screen');
+	show.style.display = 'block';
+	let hidden = document.getElementById('home-screen');
+	hidden.style.display = 'none';
+
 }
 function changeDisplayForm()
 {
-	var days = daysCalculate();
-	days = Math.round(days);
-	var dateResult  = Calculate();
+	var days = Math.round(daysCalculate());
+	var dateResult = Calculate();
 	var result = document.getElementById("result");
 	var text = result.innerHTML;
 	if(text == dateResult)
--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,19 @@
 function displayMenu()
 {
-    var nav = document.getElementsByTagName('nav');
-    var menuIcon = document.getElementById('menu-icon');
-    if(nav[0].style.display == 'block')
+    let nav = document.getElementById("nav");
+    let menuIcon = document.getElementById("menu-icon");
+    if("block" == nav.style.display)
     {
-        nav[0].style.display = 'none';
-        menuIcon.style.transform = 'rotate(0deg)';
-        menuIcon.style.left = '0px';
+        nav.style.display = "none";
+        menuIcon.style.transform = "rotate(0deg)";
+        menuIcon.style.transition = "all 0.5s ease-out";
+        menuIcon.style.left = "0px";
     }
     else
     {
-        nav[0].style.display = 'block';
-        menuIcon.style.transform = 'rotate(90deg)';
-        menuIcon.style.left = '20%';
+        nav.style.display = "block";
+        menuIcon.style.transform = "rotate(90deg)";
+        menuIcon.style.left = "20%";
     }
     
 }
@@ -20,70 +21,53 @@ function displayMenu()
 function display1stSubMenu()
 {
     var ulIcon = document.getElementsByClassName("ul-icons");
-    var subMenu = document.getElementsByClassName('sub-menu');
-    if(subMenu[0].style.display == 'block')
+    var subMenu = document.getElementsByClassName("sub-menu");
+    if(subMenu[0].style.display == "block")
     {
-        subMenu[0].style.display = 'none';
-        ulIcon[0].style.transform = 'rotate(0deg)';
+        subMenu[0].style.display = "none";
+        ulIcon[0].style.transform = "rotate(0deg)";
     }
     else
     {
-        subMenu[0].style.display = 'block';
-        ulIcon[0].style.transform = 'rotate(90deg)';
+        subMenu[0].style.display = "block";
+        ulIcon[0].style.transform = "rotate(90deg)";
     }
     
 }
 function display2ndSubMenu()
 {
     var ulIcon = document.getElementsByClassName("ul-icons");
-    var subMenu = document.getElementsByClassName('sub-menu');
-    if(subMenu[1].style.display == 'block')
+    var subMenu = document.getElementsByClassName("sub-menu");
+    if(subMenu[1].style.display == "block")
     {
-        subMenu[1].style.display = 'none';
-        ulIcon[1].style.transform = 'rotate(0deg)';
+        subMenu[1].style.display = "none";
+        ulIcon[1].style.transform = "rotate(0deg)";
     }
     else
     {
-        subMenu[1].style.display = 'block';
-        ulIcon[1].style.transform = 'rotate(90deg)';
+        subMenu[1].style.display = "block";
+        ulIcon[1].style.transform = "rotate(90deg)";
     }
         
 }
-function calculate()
-{
-    var d = 0;
-    while(d<10)
-    {
-        if(d % 2 != 0)
-        {
-            d += 2;
-        }
-        else
-        {
-            d -= 2;
-        }
-    }
-    alert(d);
-}
 function changeBackgroundToLoveImg()
-{   
-    calculate();
+{
     document.body.style.background = " url(images/L.O.V.E.jpeg) no-repeat"; 
-    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundSize = "cover";
 }
 function changeBackgroundToGardenImg()
 {
     document.body.style.background = "url(images/garden-background.jpeg) no-repeat 100% 100%"; 
-    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundSize = "cover";
 }
 // window.onclick = function hideMenu()
 // {
-//     var nav = document.getElementsByTagName('nav');
+//     var nav = document.getElementsByTagName("nav");
 //     document.onclick = function(div)
 //     {
 //         if(div.target !== nav[0])
 //         {
-//             nav[0].style.display = 'none';
+//             nav[0].style.display = "none";
 //         }
 //     };
 // }
